5f6b01fc4c117a2c7ce5c2661fbef47d00132126
JBank25 <urbankjoshua1@gmail.com>
2024-04-30 20:58:08 -0700

    Some work on types

--- value.h
+++ value.h
@@ -2,6 +2,36 @@
 
 #include "common.h"
 
+// Data types we'll support
+typedef enum
+{
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER,
+} ValueType;
+
+// TAGGED union
+typedef struct
+{
+    ValueType type; // the type tag
+    union           // union containing underlying value
+    {
+        bool boolean;
+        double number;
+    } as; // reads nicely could be as number, as boolean, as string, etc
+} Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 typedef double Value;
 
 /**
