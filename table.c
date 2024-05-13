c4532fa7037c3ab4e322747f5f0458ab0333e5e1
JBank25 <urbankjoshua1@gmail.com>
2024-05-13 06:36:20 -0700

    Hash function added, func for setting a new key/val in hash table

--- table.c
+++ table.c
@@ -18,3 +18,28 @@ void freeTable(Table *table)
     FREE_ARRAY(Entry, table->entries, table->capacity);
     initTable(table);
 }
+
+/**
+ * @brief Adds a given key/value to a hash table. If the key already exists
+ * in the table then it is overridden
+ *
+ * @param table - hash table to add new key/value to
+ * @param key - Key of value going into hash table
+ * @param value - Value going into hash table
+ *
+ * @return true if the key creates a new entry into the hash table
+ * false otherwise.
+ */
+bool tableSet(Table *table, ObjString *key, Value value)
+{
+    // finds entry in hashtable, this var will hold a ptr to that memory location
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+
+    bool isNewKey = (entry->key == NULL);
+    if (isNewKey)
+        table->count++;
+
+    entry->key = key;     // populate the hash table with the key
+    entry->value = value; // and value
+    return isNewKey;
+}
