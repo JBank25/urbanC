7d0e71fe7f9f69d9c6aba7d2c57a7f4ef44a2a69
JBank25 <urbankjoshua1@gmail.com>
2024-05-13 20:40:57 -0700

    findEntry for hash table implementation

--- table.c
+++ table.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Should this go into header? Maybe not, only user in this file
+#define TABLE_MAX_LOAD 0.75
+
 void initTable(Table *table)
 {
     table->count = 0;
@@ -17,6 +20,35 @@ void freeTable(Table *table)
 {
     FREE_ARRAY(Entry, table->entries, table->capacity);
     initTable(table);
+}
+
+/**
+ * Finds an entry in the given array of entries with the specified key.
+ * This function will be used to both find existing entries in the hash
+ * table and for deciding where to insert new ones.
+ *
+ * @param entries   The array of entries to search.
+ * @param capacity  The capacity of the array.
+ * @param key       The key to search for.
+ * @return          A pointer to the found entry OR ptr to empty entry which can then be populated.
+ *                  caller may want to read the value, update it, or create a new entry
+ */
+static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
+{
+    // modulo to map key's hash code into an index within the arrays bounds
+    uint32_t index = key->hash % capacity;
+    for (;;) // <- loop to probe in event of collision, no infinite loop due load factor checking
+    {
+        // grab the entry
+        Entry *entry = &entries[index];
+        // if key already exists OR the entry is empty, return it to be populated, updated, etc
+        if (entry->key == key || entry->key == NULL)
+        {
+            return entry;
+        }
+        // if collision occurred, start probing
+        index = (index + 1) % capacity;
+    }
 }
 
 /**
@@ -32,12 +64,22 @@ void freeTable(Table *table)
  */
 bool tableSet(Table *table, ObjString *key, Value value)
 {
+    // TABLE_MAX_LOAD is how we manage the tables load factor. Could be say
+    // .75 so we only increase when the array is at least 75% full
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD)
+    {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+
     // finds entry in hashtable, this var will hold a ptr to that memory location
     Entry *entry = findEntry(table->entries, table->capacity, key);
 
-    bool isNewKey = (entry->key == NULL);
+    bool isNewKey = (entry->key == NULL); // true if the key/val is new
     if (isNewKey)
-        table->count++;
+    {
+        table->count++; // only increase count when it is a new ley/val
+    }
 
     entry->key = key;     // populate the hash table with the key
     entry->value = value; // and value
