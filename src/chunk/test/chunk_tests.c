d7635081b1d65d6fd3086e0588b5a0877cdfb3fd
JBank25 <urbankjoshua1@gmail.com>
2025-01-24 14:15:33 -0600

    Think unity testing works

--- src/chunk/test/chunk_tests.c
+++ src/chunk/test/chunk_tests.c
@@ -1,6 +1,26 @@
+#include "unity.h"
+#include "unity/unity.h"
+#include "unity/unity_internals.h"
+#include "unity_internals.h"
 
+void setUp(void) {
+    // set stuff up here
+}
 
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void dummy(void)
+{
+  TEST_ASSERT_EQUAL(1, 2);
+}
+ 
 int main(void)
 {
-    return 0;
+    UNITY_BEGIN();
+
+  RUN_TEST(dummy);
+ 
+  return UNITY_END();
 }
