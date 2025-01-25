47fafae0ca3ab8af6747214b633605f4757e7198
JBank25 <urbankjoshua1@gmail.com>
2025-01-24 19:42:51 -0600

    Changes to testing

--- src/chunk/Module.cmake
+++ src/chunk/Module.cmake
@@ -1,3 +1,3 @@
 set(MODULE_TARGET "Chunk")
 set(MODULE_TEST_TARGET "ChunkTests")
-set(MODULE_TEST_SUITE "ModuleChunkTests")
+set(MODULE_TEST_SUITE "Module_ChunkTests")
--- src/chunk/test/chunk_tests.c
+++ src/chunk/test/chunk_tests.c
@@ -1,31 +1,45 @@
 #include "chunk.h"
+#include "memory.h"
+#include "value.h"
 
 #include "unity.h"
 #include "unity/unity.h"
 #include "unity/unity_internals.h"
 #include "unity_internals.h"
 
-void setUp(void) {
-    // set stuff up here
-}
-
-void tearDown(void) {
-    // clean stuff up here
-}
-
-void dummy(void)
+void setUp(void) 
 {
-    Chunk testChunk = {0};
-    testChunk.count = 1;
+}
+
+void tearDown(void) 
+{
+}
+
+void Test_Chunk_InitChunk(void)
+{
+    Chunk testChunk = {};
     Chunk_InitChunk(&testChunk);
     TEST_ASSERT_EQUAL(testChunk.count, 0);
+}
+
+void Test_Chunk_AddConstant(void)
+{
+    Chunk testChunk = {0};
+    Value testVal = {.type = VAL_NUMBER, .as = {.number = 0xF0F0}};
+
+    int constantIdx = Chunk_AddConstant(&testChunk, testVal);
+    TEST_ASSERT_EQUAL(testChunk.constants.count , 1);
+    TEST_ASSERT_EQUAL(testChunk.constants.capacity, MIN_ARR_THRESHOLD);
+    TEST_ASSERT_EQUAL(AS_NUMBER(testChunk.constants.values[constantIdx]), AS_NUMBER(testVal));
+    TEST_ASSERT_EQUAL(constantIdx, 0);
 }
  
 int main(void)
 {
     UNITY_BEGIN();
 
-    RUN_TEST(dummy);
+    RUN_TEST(Test_Chunk_InitChunk);
+    RUN_TEST(Test_Chunk_AddConstant);
  
   return UNITY_END();
 }
